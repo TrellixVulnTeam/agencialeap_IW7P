254cda3d544aebe03ef94cab3e85191a00231c3a
Eloy Pellegrino <eloypellegrino@hotmail.com>
2022-02-22 18:44:43 -0300

    Leap Orange

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
-const checkbox = document.querySelector("#checkbox1");
-const background = document.querySelectorAll("body, .containerHeader");
-const backgroundSecondary = document.querySelectorAll(".about, .containerProject");
-const backgroundImage = document.querySelector(".iphoneContainer");
-const text = document.querySelectorAll("h1, h2, h3, h4, p, a");
-const menuMobile = document.querySelector(".menuMobile i");
-const menuMobileList = document.querySelector(".list");
-const itemMenu = document.querySelectorAll(".itemMenu");
+const checkbox = document.querySelector('#checkbox1');
+const background = document.querySelectorAll('body, .containerHeader');
+const backgroundSecondary = document.querySelectorAll(
+    '.about, .containerProject'
+);
+const backgroundImage = document.querySelector('.iphoneContainer');
+const text = document.querySelectorAll('h1, h2, h3, h4, p, a');
+const menuMobile = document.querySelector('.menuMobile i');
+const menuMobileList = document.querySelector('.list');
+const itemMenu = document.querySelectorAll('.itemMenu');
 
-checkbox.addEventListener("click", () => {
+checkbox.addEventListener('click', () => {
     for (item of background) {
-        item.classList.add("transition");
-        item.classList.toggle("darkBackground");
+        item.classList.add('transition');
+        item.classList.toggle('darkBackground');
     }
     for (item of backgroundSecondary) {
-        item.classList.add("transition");
-        item.classList.toggle("darkBackgroundSecondary");
+        item.classList.add('transition');
+        item.classList.toggle('darkBackgroundSecondary');
     }
     for (item of text) {
-        item.classList.add("transition");
-        item.classList.toggle("darkText");
+        item.classList.add('transition');
+        item.classList.toggle('darkText');
     }
-    backgroundImage.classList.toggle("darkBackgroundImage");
-    menuMobile.classList.toggle("darkText");
-    menuMobileList.classList.toggle("darkBackground");
-    background[1].classList.toggle("darkContainerHeader")
+    backgroundImage.classList.toggle('darkBackgroundImage');
+    menuMobile.classList.toggle('darkText');
+    menuMobileList.classList.toggle('darkBackground');
+    background[1].classList.toggle('darkContainerHeader');
 });
 checkbox.checked = false;
 
@@ -35,17 +37,17 @@ function menuClick() {
     // } else {
     //     menuMobile.classList.replace("fa-times", "fa-bars");
     // }
-    menuMobileList.classList.toggle("-active");
+    menuMobileList.classList.toggle('-active');
 }
 
-menuMobile.addEventListener("click", menuClick);
+menuMobile.addEventListener('click', menuClick);
 
 itemMenu.forEach((item) => {
-    item.addEventListener("click", () => {
-        menuMobileList.classList.toggle("-active");
-        menuMobileList.classList.toggle("list closeMenuMobile");
-        menuMobile.classList.replace("fa-times", "fa-bars");
+    item.addEventListener('click', () => {
+        menuMobileList.classList.toggle('-active');
+        menuMobileList.classList.toggle('list closeMenuMobile');
+        menuMobile.classList.replace('fa-times', 'fa-bars');
     });
 });
 
-checkbox.click()
+// checkbox.click();
